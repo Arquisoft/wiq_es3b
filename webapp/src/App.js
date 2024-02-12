76d608f7d19e5530a584851d926f338a0965f298
UO276976 <UO276976@uniovi.es>
2024-02-12 10:39:08 +0100

    Updating Participation component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -5,9 +5,15 @@ import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
+import Participation from './components/Participation';
 
 function App() {
+  const [showComponent, setShowComponent] = useState(0);
   const [showLogin, setShowLogin] = useState(true);
+
+  const handleToggleComponentView = () => {
+    setShowComponent(showComponent);
+  }
 
   const handleToggleView = () => {
     setShowLogin(!showLogin);
@@ -19,6 +25,7 @@ function App() {
       <Typography component="h1" variant="h5" align="center" sx={{ marginTop: 2 }}>
         Welcome to the 2024 edition of the Software Architecture course
       </Typography>
+      <Participation />
       {showLogin ? <Login /> : <AddUser />}
       <Typography component="div" align="center" sx={{ marginTop: 2 }}>
         {showLogin ? (
