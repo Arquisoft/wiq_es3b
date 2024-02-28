f71c0cdd669ee27e4acc73d9fd9c5e8ece3c22cb
UO287747 <uo287747@uniovi.es>
2024-02-29 00:11:07 +0100

    Otro test fix

--- webapp/src/test/Participation.test.js
+++ webapp/src/test/Participation.test.js
@@ -12,17 +12,4 @@ describe('Participation component', () => {
     expect(titleText).toBeInTheDocument();
 
   });
-
-  test('clicking "Menu" button calls goTo function with correct argument', () => {
-    const goToMock = jest.fn();
-    render(<Participation goTo={goToMock} />);
-
-    render(<Nav goTo={goToMock} />)
-
-    // Simular clic en el botón "Menu"
-    fireEvent.click(screen.getByText('Volver al Menú'));
-
-    // Verificar que la función goTo se llamó con el argumento correcto (en este caso, 1)
-    expect(goToMock).toHaveBeenCalledWith(1);
-  });
 });
