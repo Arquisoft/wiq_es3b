19182b01fcd838c9fd2381c807c4ef659e12b1ca
UO287747 <uo287747@uniovi.es>
2024-02-29 22:11:45 +0100

    Fix test

--- webapp/src/test/Game.test.js
+++ webapp/src/test/Game.test.js
@@ -1,42 +1,42 @@
 import React from 'react';
 import { render, fireEvent, waitFor } from '@testing-library/react';
-import { Game } from '../components/Game'; // Importamos el componente correcto
+import { Game } from '../components/Game';
 
 describe('Question Component', () => {
-
-  test('fetches and displays a question with options', async () => {
-    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />); // Renderizamos el componente correcto y simulamos la función goTo
-
-    // Esperamos a que se muestre la pregunta
+  test('renders question and options', async () => {
+    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />);
+    
     await waitFor(() => {
       expect(getByText('Question: 1')).toBeInTheDocument();
     });
 
-    // Esperamos a que se muestren las opciones
     await waitFor(() => {
-      expect(getAllByRole('listitem')).toHaveLength(4); // Asumimos que hay 4 opciones (la pregunta más 3 opciones incorrectas)
+      expect(getAllByRole('listitem')).toHaveLength(4);
     });
   });
 
   test('selects an option and displays feedback', async () => {
-    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />); // Renderizamos el componente correcto y simulamos la función goTo
+    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />);
 
-    // Esperamos a que se muestre la pregunta
     await waitFor(() => {
       expect(getByText('Question: 1')).toBeInTheDocument();
     });
 
-    // Seleccionamos una opción (simulamos hacer clic)
     const options = getAllByRole('listitem');
-    fireEvent.click(options[0]); // Suponemos que la primera opción es la correcta
+    fireEvent.click(options[0]); // Select the first option
 
-    // Esperamos a que se muestre el botón Next
     await waitFor(() => {
       expect(getByText('Next')).toBeInTheDocument();
     });
 
   });
 
-  // Agrega más tests según sea necesario para cubrir otros comportamientos del componente
-});
+  test('handles clicking "Next" button', async () => {
+    const { getByText, queryByText } = render(<Game goTo={() => {}} />);
+    
+    await waitFor(() => {
+      expect(getByText('Question: 1')).toBeInTheDocument();
+    });
 
+  });
+});
