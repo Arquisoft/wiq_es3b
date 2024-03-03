4c5f51df743b3689cdd62dfe719546b331a3b07e
UO287747 <uo287747@uniovi.es>
2024-03-03 18:35:45 +0100

    Nuevos test

--- webapp/src/test/Game.test.js
+++ webapp/src/test/Game.test.js
@@ -3,39 +3,60 @@ import { render, fireEvent, waitFor } from '@testing-library/react';
 import { Game } from '../components/Game';
 
 describe('Question Component', () => {
-  test('renders question and options', async () => {
-    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />);
-    
-    await waitFor(() => {
-      
-    });
-
-    await waitFor(() => {
-      expect(getAllByRole('listitem')).toHaveLength(4);
-    });
-  });
-
   test('selects an option and displays feedback', async () => {
-    const { getByText, getAllByRole } = render(<Game goTo={() => {}} />);
+    const { getByText, getAllByRole, container } = render(<Game goTo={() => {}} />);
 
     await waitFor(() => {
-      
+      expect(getByText('Question: 0')).toBeInTheDocument();
     });
-
-    
 
     await waitFor(() => {
-      
+      const divElements = container.querySelectorAll('div');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('p');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('h4');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('ul');
+      expect(divElements.length).toBeGreaterThan(0);
     });
 
+    const options = getAllByRole('listitem');
+    fireEvent.click(options[0]);
+
+    await waitFor(() => {
+      expect(getByText('Next')).toBeInTheDocument();
+    });
   });
 
   test('handles clicking "Next" button', async () => {
-    const { getByText, queryByText } = render(<Game goTo={() => {}} />);
+    const { getByText, queryByText, container } = render(<Game goTo={() => {}} />);
     
     await waitFor(() => {
-      
+      expect(getByText('Question: 0')).toBeInTheDocument();
     });
 
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('div');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('p');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('h4');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
+    await waitFor(() => {
+      const divElements = container.querySelectorAll('ul');
+      expect(divElements.length).toBeGreaterThan(0);
+    });
   });
 });
