4548f4b5e73998052dc8ad051ffdc820cfbc6aec
gabigarciam <125141315+gabigarciam@users.noreply.github.com>
2024-03-29 16:10:03 +0100

    Cambio fútbol

--- question_generator/sports/football/footballTemplates.js
+++ question_generator/sports/football/footballTemplates.js
@@ -1,36 +1,43 @@
 const footballQuestions=require('./footballQuestions');
+const console = require('console')
 const footballQuery=footballQuestions.getInstance();
 function loadData(){
     footballQuery.loadData();
 }
 const templates=[
+    /*async ()=>
+    {
+        const results = await footballQuery.doQuestion('country', 4);
+        console.log("country");
+        return{
+            "question":"Which football team is from?",
+            "question_param":results.question_param,
+            "correct":results.correct,
+            "incorrects":results.incorrects
+        }
+    },*/
     async ()=>
     {
-        const results = await footballQuery.getTeamForCountry();
+        const results = await footballQuery.doQuestion('coach', 4);
+        console.log("coach");
         return{
-            "question":"Which football team is from " + results.country + "? ",
+            "question":"Which team trains? ",
+            "question_param":results.question_param,
             "correct":results.correct,
             "incorrects":results.incorrects
         }
     },
     async ()=>
     {
-        const results = await footballQuery.getTeamForCoach();
+        const results = await footballQuery.doQuestion('stadium', 4);
+        console.log("stadium");
         return{
-            "question":"Which team trains " + results.coach + "? ",
+            "question":"Which team plays in? ",
+            "question_param":results.question_param,
             "correct":results.correct,
             "incorrects":results.incorrects
         }
-    }/*,
-    async ()=>
-    {
-        const results = await footballQuery.getTeamForCoach();
-        return{
-            "question":"Which team plays in " + results.stadium + "? ",
-            "correct":results.correct,
-            "incorrects":results.incorrects
-        }
-    }*/
+    }
 ]
 module.exports.getRandomQuestion = () => templates[Math.floor(Math.random()*templates.length)]();
 module.exports.loadData = ()=>loadData();
