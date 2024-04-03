722b046825396d2e38771c4b769c24fe6e93cdc0
Alfonso <UO276976@uniovi.es>
2024-04-03 23:00:02 +0200

    format

--- webapp/src/components/PostGame.jsx
+++ webapp/src/components/PostGame.jsx
@@ -1,4 +1,5 @@
-import { Card, Typography } from "@mui/material"
+import React from 'react';
+import { Card, Typography } from "@mui/material";
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -15,40 +16,34 @@ export const PostGame = () => {
     };
 
     return (
-
         <main className='preguntas'>
-        <div>
-            <Typography sx={{ textAlign: 'center', fontSize:'2.2em' }}>Fin del juego</Typography>
-            <Card>
-            <TableContainer component={Paper}>
-                <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                    <TableBody>
-          
-                        <TableRow key={"Preguntas acertadas"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ fontSize:'1.3em' }}>Preguntas acertadas</TableCell>
-                            <TableCell sx={{ fontSize:'1.3em' }} align="right">{localStorage.getItem("pAcertadas")}</TableCell>
-                        </TableRow>
-
-                        <TableRow key={"Preguntas falladas"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ fontSize:'1.3em' }}>Preguntas falladas</TableCell>
-                            <TableCell sx={{ fontSize:'1.3em' }} align="right">{localStorage.getItem("pFalladas")}</TableCell>
-                        </TableRow>
-
-                        <TableRow key={"Tiempo usado"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ fontSize:'1.3em' }}>Tiempo usado</TableCell>
-                            <TableCell sx={{ fontSize:'1.3em' }} align="right">{formatTiempo(localStorage.getItem("tiempoUsado"))}</TableCell>
-                        </TableRow>
-
-                        <TableRow key={"Tiempo restante"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                            <TableCell sx={{ fontSize:'1.3em' }}>Tiempo restante</TableCell>
-                            <TableCell sx={{ fontSize:'1.3em' }} align="right">{formatTiempo(localStorage.getItem("tiempoRestante"))}</TableCell>
-                        </TableRow>
-          
-                    </TableBody>
-                </Table>
-            </TableContainer>
-            </Card>
-        </div>
+            <div>
+                <Typography sx={{ textAlign: 'center', fontSize:'2.2em' }}>Fin del juego</Typography>
+                <Card>
+                    <TableContainer component={Paper}>
+                        <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                            <TableBody>
+                                <TableRow key={"Preguntas acertadas"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                    <TableCell sx={{ fontSize:'1.3em' }}>Preguntas acertadas</TableCell>
+                                    <TableCell sx={{ fontSize:'1.3em' }} align="right">{localStorage.getItem("pAcertadas")}</TableCell>
+                                </TableRow>
+                                <TableRow key={"Preguntas falladas"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                    <TableCell sx={{ fontSize:'1.3em' }}>Preguntas falladas</TableCell>
+                                    <TableCell sx={{ fontSize:'1.3em' }} align="right">{localStorage.getItem("pFalladas")}</TableCell>
+                                </TableRow>
+                                <TableRow key={"Tiempo usado"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                    <TableCell sx={{ fontSize:'1.3em' }}>Tiempo usado</TableCell>
+                                    <TableCell sx={{ fontSize:'1.3em' }} align="right">{formatTiempo(localStorage.getItem("tiempoUsado"))}</TableCell>
+                                </TableRow>
+                                <TableRow key={"Tiempo restante"} sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                                    <TableCell sx={{ fontSize:'1.3em' }}>Tiempo restante</TableCell>
+                                    <TableCell sx={{ fontSize:'1.3em' }} align="right">{formatTiempo(localStorage.getItem("tiempoRestante"))}</TableCell>
+                                </TableRow>
+                            </TableBody>
+                        </Table>
+                    </TableContainer>
+                </Card>
+            </div>
         </main>
     )
 }
