dce825e03d888deb52619b480850d28801d35d9c
Alfonso <UO276976@uniovi.es>
2024-04-04 11:05:10 +0200

    Game.test

--- webapp/src/test/Game.test.js
+++ webapp/src/test/Game.test.js
@@ -50,15 +50,18 @@ describe('Game component', () => {
           <Game goTo={mockGoTo} />
         </SessionProvider>
       );
-
+  
       await waitFor(() => {
-        expect(screen.getByText(/Question/i)).toBeInTheDocument();
+        expect(screen.getByText(mockQuestions[0].question)).toBeInTheDocument();
       });
-
-      expect(screen.getAllByText(/./i)).toHaveLength(4); // Verifica que haya 4 opciones
+  
+      expect(screen.getByText(mockQuestions[0].correct)).toBeInTheDocument();
+      expect(screen.getByText(mockQuestions[0].incorrects[0])).toBeInTheDocument();
+      expect(screen.getByText(mockQuestions[0].incorrects[1])).toBeInTheDocument();
+      expect(screen.getByText(mockQuestions[0].incorrects[2])).toBeInTheDocument();
     });
   });
-
+  
   it('handles option selection correctly', async () => {
     await act(async () => {
       render(
@@ -66,20 +69,20 @@ describe('Game component', () => {
           <Game goTo={mockGoTo} />
         </SessionProvider>
       );
-
+  
       await waitFor(() => {
         expect(screen.getByText(mockQuestions[0].question)).toBeInTheDocument();
       });
-
+  
       // Seleccionar la opción correcta
       fireEvent.click(screen.getByText(mockQuestions[0].correct));
       expect(screen.getByText(mockQuestions[0].correct).parentElement).toBeInTheDocument();
-
+  
       // Seleccionar una opción incorrecta
       fireEvent.click(screen.getByText(mockQuestions[0].incorrects[0]));
       expect(screen.getByText(mockQuestions[0].incorrects[0]).parentElement).toBeInTheDocument();
     });
-  });
+  });  
 
   // Test para verificar que el juego finaliza cuando se alcanza el número máximo de preguntas
   test('El juego finaliza correctamente cuando se alcanza el número máximo de preguntas', async () => {
