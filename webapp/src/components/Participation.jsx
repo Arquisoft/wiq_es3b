228a9b04e00edd25b5e729e244155176d9185af2
Alfonso <UO276976@uniovi.es>
2024-04-21 17:09:15 +0200

    updating participation

--- webapp/src/components/Participation.jsx
+++ webapp/src/components/Participation.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect, useContext } from 'react';
 import axios from 'axios';
+import '../css/participation.css';
 import { SessionContext } from '../SessionContext';
+import { FormattedMessage } from 'react-intl';
+
+import { PieChart, Pie, Tooltip } from 'recharts';
 
 const gatewayUrl = process.env.REACT_APP_API_ENDPOINT || "http://localhost:8000";
 
 export const Participation = ({ goTo }) => {
-  const { sessionData } = useContext(SessionContext); // Obtener datos de sesión del contexto
+  const { sessionData } = useContext(SessionContext);
   const [participationData, setParticipationData] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -13,7 +17,7 @@ export const Participation = ({ goTo }) => {
     // Realizar la solicitud al servidor para obtener los datos de participación
     const fetchData = async () => {
       try { 
-        const response = await axios.get(`${gatewayUrl}/getParticipation/${sessionData.userId}`); 
+        const response = await axios.get(`${gatewayUrl}/getParticipation/${sessionData.userId}`);
         if (response.status === 204) {
           setLoading(false);
         } else {
@@ -32,23 +36,44 @@ export const Participation = ({ goTo }) => {
   }, [sessionData]);
 
   return (
-    <main>
+    <main className="Participation-container">
       <div>
-        <h1>Participación</h1>
-        {loading ? (
-          <p>{participationData ? 'Cargando datos de participación...' : 'No hay datos de participación'}</p>
+      <h1 className="Participation-title"><FormattedMessage id="participationTitle" /></h1>
+
+      {loading ? (
+        <p><FormattedMessage id={participationData ? "loadingData" : "noParticipationData"} className="Participation-text"/></p>
+      ) : (
+        participationData !== null ? (
+          <div className="Participation-text">
+            <p><FormattedMessage id="totalGames" />: {participationData.totalGames}</p>
+            <p><FormattedMessage id="correctAnswers" />: {participationData.correctAnswers}</p>
+            <p><FormattedMessage id="incorrectAnswers" />: {participationData.incorrectAnswers}</p>
+            <p><FormattedMessage id="totalTime" />: {participationData.totalTime} <FormattedMessage id="seconds" /></p>
+
+            <PieChart width={400} height={400}>
+              <Pie
+                data={[
+                  { name: <FormattedMessage id="correctAnswers" />, value: participationData.correctAnswers },
+                  { name: <FormattedMessage id="incorrectAnswers" />, value: participationData.incorrectAnswers }
+                ]}
+                dataKey="value"
+                nameKey="name"
+                cx="50%"
+                cy="50%"
+                outerRadius={80}
+                label
+                labelLine={false}
+              >
+                <Cell fill="#82ca9d" /> {/* Color verde para Respuestas Correctas */}
+                <Cell fill="#ff6b6b" /> {/* Color rojo para Respuestas Incorrectas */}
+              </Pie>
+              <Tooltip />
+            </PieChart>
+          </div>
         ) : (
-          participationData !== null ? (
-            <div>
-              <p>Número de partidas jugadas: {participationData.totalGames}</p>
-              <p>Preguntas acertadas: {participationData.correctAnswers}</p>
-              <p>Preguntas falladas: {participationData.incorrectAnswers}</p>
-              <p>Tiempo total jugando: {participationData.totalTime} segundos</p>
-            </div>
-          ) : (
-            <p>No hay datos de participación</p>
-          )
-        )}
+          <p><FormattedMessage id="noParticipationData" className="Participation-text"/></p>
+        )
+      )}
       </div>
     </main>
   );
