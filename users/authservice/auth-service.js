52698731abc5699382e5daf2a853d598c626e2d0
Miguel Méndez Murias <uo287687@uniovi.es>
2024-04-28 17:26:24 +0200

    Security issue fixed

--- users/authservice/auth-service.js
+++ users/authservice/auth-service.js
@@ -32,7 +32,7 @@ app.post('/login', async (req, res) => {
     const { username, password } = req.body;
 
     // Find the user by username in the database
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username:username.toString() });
 
     // Check if the user exists and verify the password
     if (user && await bcrypt.compare(password, user.password)) {
